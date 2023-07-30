7de00f9881b6f924c8a70135384f065811f8d941
Jacob Golden <golden.songs@gmail.com>
2023-07-30 10:57:46 -0700

    trigger actions

--- src/download_model.py
+++ src/download_model.py
@@ -3,6 +3,7 @@ from huggingface_hub import snapshot_download
 
 # Get the hugging face token
 HUGGING_FACE_HUB_TOKEN = os.environ['HUGGING_FACE_HUB_TOKEN']
+#testing out ehartford/based-13b
 HF_MODEL = "ehartford/based-13b"
 
 # Download the 7B
